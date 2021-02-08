19c61b9e5227d2835455d0b0ec323c8f27c9c18d
Georgie Horth <georgiehorth@gmail.com>
2021-02-08 10:24:56 +0000

    Passing int truncates the decimal places. Corrected code now passes all kata tests

--- Python_7_Kata/middle_letter/middle_letter.py
+++ Python_7_Kata/middle_letter/middle_letter.py
@@ -1,9 +1,7 @@
 def middle_letter(str):
-    if len(str) == 1:
-        return str
     if (len(str) % 2 == 0):
-        return "it"
+        s = int(len(str)/2)
+        return str[s-1] + str[s]
     else:
-       s = len(str)/2
-       s = round(s) 
-       return str[s-1]
+       s = int(len(str)/2)
+       return str[s]
--- Python_7_Kata/middle_letter/test_middle_letter.py
+++ Python_7_Kata/middle_letter/test_middle_letter.py
@@ -19,3 +19,8 @@ def test_returns_correct_letter_from_odd_string():
     solution = middle_letter("testing")
 
     assert solution == "t"
+
+def test_returns_middle_two_letters_from_even_string():
+    solution = middle_letter("middle")
+
+    assert solution == "dd"
