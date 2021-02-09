53c5633930d0962e017f38bbeb9783ade6db3fef
Georgie Horth <georgiehorth@gmail.com>
2021-02-09 09:52:26 +0000

    Logic for three names passing

--- Python_6_Kata/Who_Likes_it/likes.py
+++ Python_6_Kata/Who_Likes_it/likes.py
@@ -1,2 +1,7 @@
 def likes(names):
-    return names[0] + " likes this"
+    if len(names) == 1:
+        return names[0] + " likes this"
+    elif len(names) == 2:
+        return names[0] + " and " + names[1] + " like this"
+    else:
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this"
--- Python_6_Kata/Who_Likes_it/test_likes.py
+++ Python_6_Kata/Who_Likes_it/test_likes.py
@@ -8,3 +8,11 @@ def test_returns_a_single_name_likes():
 def test_returns_a_different_single_name():
     solution = likes(["Bob"])
     assert solution == "Bob likes this"
+
+def test_returns_two_name_likes():
+    solution = likes(['Jacob', 'Alex'])
+    assert solution == 'Jacob and Alex like this'
+
+def test_returns_three_name_likes():
+    solution = likes(['Max', 'John', 'Mark'])
+    assert solution == 'Max, John and Mark like this'
