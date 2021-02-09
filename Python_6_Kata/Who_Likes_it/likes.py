b6308946abc66511f1c7efe770bc69910cf2d2b5
Georgie Horth <georgiehorth@gmail.com>
2021-02-09 10:22:56 +0000

    logic passing, researching for refactoring solutions

--- Python_6_Kata/Who_Likes_it/likes.py
+++ Python_6_Kata/Who_Likes_it/likes.py
@@ -1,7 +1,12 @@
 def likes(names):
-    if len(names) == 1:
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
         return names[0] + " likes this"
     elif len(names) == 2:
         return names[0] + " and " + names[1] + " like this"
-    else:
+    elif len(names) == 3:
         return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+    elif len(names) >= 4:
+        additional_likes = str(len(names) - 2)
+        return names[0] + ", " + names[1] + " and " + additional_likes + " others like this"
--- Python_6_Kata/Who_Likes_it/test_likes.py
+++ Python_6_Kata/Who_Likes_it/test_likes.py
@@ -16,3 +16,11 @@ def test_returns_two_name_likes():
 def test_returns_three_name_likes():
     solution = likes(['Max', 'John', 'Mark'])
     assert solution == 'Max, John and Mark like this'
+
+def test_returns_4_names_likes():
+    solution = likes(['Alex', 'Jacob', 'Mark', 'Max'])
+    assert solution == 'Alex, Jacob and 2 others like this'
+
+def test_can_handle_an_empty_list():
+    solution = likes([])
+    assert solution == "no one likes this"
